7ab957bda39acdd6aa956a77c94c34d4a50e56a6
june <eptm917@gmail.com>
2022-11-01 14:10:05 +0900

    init-project2

---
+++ IOS-Practice2/IOS-Practice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-Practice2
+//
+//  Created by June on 2022/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
